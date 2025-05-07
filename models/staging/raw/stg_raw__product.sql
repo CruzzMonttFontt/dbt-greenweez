d0934c189037e6f31089c6095c728d1eaae52042
cruzmontfont@gmail.com <cruzmontfont@gmail.com>
2025-05-07 14:57:28 +0000

    challenge 1 y parte del 2

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,23 +1,15 @@
--- stg_raw__product.sql
-
-with
-
-source as (
-
+with source as (
     select * from {{ source('raw', 'product') }}
-
 ),
 
 renamed as (
 
     select
         products_id,
-        name,
-        category,
-        CAST(purchase_price AS FLOAT64) AS purchase_price
+        CAST(purchse_price AS FLOAT64) as purchase_price
 
     from source
-
 )
 
 select * from renamed
+
